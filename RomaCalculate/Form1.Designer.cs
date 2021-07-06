fcfe4c253495b3cb18ab4c8a88cca992479ad0ed
ilyas161 <ilyas.hydyrov@gmail.com>
2021-07-06 16:30:14 +0400

    Добавьте файлы проекта.

---
+++ RomaCalculate/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace RomaCalculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
+            this.button25 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 254);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(0, 250);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(70, 62);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "M";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(67, 250);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(70, 62);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "+";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(132, 250);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(70, 62);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "-";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(199, 250);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(70, 62);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "/";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(264, 250);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(70, 62);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "C";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(0, 309);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(70, 62);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "L";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(67, 309);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(70, 62);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "C";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(132, 309);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(70, 62);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "D";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(199, 309);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(70, 62);
+            this.button19.TabIndex = 19;
+            this.button19.Text = "*";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(264, 309);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(70, 62);
+            this.button20.TabIndex = 20;
+            this.button20.Text = "<-";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(0, 367);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(70, 62);
+            this.button21.TabIndex = 21;
+            this.button21.Text = "I";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(67, 367);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(70, 62);
+            this.button22.TabIndex = 22;
+            this.button22.Text = "V";
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(132, 367);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(70, 62);
+            this.button23.TabIndex = 23;
+            this.button23.Text = "X";
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(199, 367);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(70, 62);
+            this.button24.TabIndex = 24;
+            this.button24.Text = "%";
+            this.button24.UseVisualStyleBackColor = true;
+            this.button24.Click += new System.EventHandler(this.button24_Click);
+            // 
+            // button25
+            // 
+            this.button25.Location = new System.Drawing.Point(264, 367);
+            this.button25.Name = "button25";
+            this.button25.Size = new System.Drawing.Size(70, 62);
+            this.button25.TabIndex = 25;
+            this.button25.Text = "=";
+            this.button25.UseVisualStyleBackColor = true;
+            this.button25.Click += new System.EventHandler(this.button25_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 425);
+            this.Controls.Add(this.button25);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = " Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button25;
+    }
+}
+
---
+++ RomaCalculate/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomaCalculate
+{
+        public partial class Form1 : Form
+        {
+            string a = "", b = "";
+            char c = '=';
+            public Form1()
+            {
+                InitializeComponent();
+                
+                this.BackColor = Color.Gray;
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+            // this.Close();
+            //  richTextBox1.Text = RomaCalculate(Translate("VII"));
+                a += "I";
+            if(b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + c + a;
+        }
+
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            a += "V";
+            if (b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + c + a;
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            a += "X";
+            if (b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + c + a;
+            //  MessageBox.Show(Convert.ToString(Translate(a)));
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            a += "L";
+            if (b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + c + a;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            a += "C";
+            if (b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + c + a;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            a += "D";
+            if (b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + c + a;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            a += "M";
+            if (b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + c + a;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            
+            c = '+';
+            richTextBox1.Text = a + c;
+            b = a;
+            a = "";
+           // if (a != "")
+          //  MessageBox.Show(Convert.ToString(Translate(a)));
+
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+           
+            c = '-';
+            richTextBox1.Text = a + c;
+            b = a;
+            a = "";
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+            
+            c = '/';
+            richTextBox1.Text = a + c;
+            b = a;
+            a = "";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            c = '*';
+            richTextBox1.Text = a + c;
+            b = a;
+            a = "";
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            c = '%';
+            richTextBox1.Text = a + c;
+            b = a;
+            a = "";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            a = "";
+            richTextBox1.Text = a + c;
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            string d = "";
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                d += a[i];
+            }
+            a = d;
+            if (b == "")
+                richTextBox1.Text = a;
+            else
+                richTextBox1.Text = b + a;
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            string rezultFinal = "";
+            switch (c)
+            {
+                case '+':
+                    rezultFinal = RomaCalculate(Translate(a) + Translate(b));
+                   // MessageBox.Show("hello world");
+                  //  MessageBox.Show(Convert.ToString(Translate(a)));
+                    break;
+                case '-':
+                    rezultFinal = RomaCalculate(Translate(b) - Translate(a));
+                   // MessageBox.Show(Convert.ToString(Translate(rezultFinal)));
+                    break;
+                case '*':
+                    rezultFinal = RomaCalculate(Translate(a) * Translate(b));
+                    break;
+                case '/':
+                    rezultFinal = RomaCalculate(Translate(b) / Translate(a));
+                    break;
+                case '%':
+                    rezultFinal = RomaCalculate(Translate(b) % Translate(a));
+                    break;
+                default:
+                   // Console.WriteLine("no such operation has been added yet");
+                    break;
+            }
+
+            richTextBox1.Text = b + c + a + "=" + rezultFinal;
+            b = "";
+            c = '=';
+            a = rezultFinal;
+        }
+        private string RomaCalculate(int ArabNumbers)
+            {
+                string rezult = "";
+                var Dict = new Dictionary<int, string>
+                {
+                    { 1, "I" },
+                    { 2, "II" },
+                    { 3, "III"},
+                    { 4, "IV"},
+                    { 5, "V" },
+                    { 6, "VI" },
+                    { 7, "VII" },
+                    { 8, "VIII" },
+                    { 9, "IX" },
+                    { 10, "X" },
+                    { 20, "XX" },
+                    { 30, "XXX" },
+                    { 40, "XL" },
+                    { 50, "L" },
+                    { 60, "LX" },
+                    { 70, "LXX" },
+                    { 80, "LXXX" },
+                    { 90, "XC" },
+                    { 100, "C" },
+                    { 200, "CC" },
+                    { 300, "CCC" },
+                    { 400, "CD" },
+                    { 500, "D" },
+                    { 600, "DC" },
+                    { 700, "DCC" },
+                    { 800, "DCCC" },
+                    { 900, "CM" },
+                    { 1000, "M" }
+                };
+               
+                foreach (var item in Dict.Keys.Reverse())
+                { 
+                    while (ArabNumbers / item >= 1)
+                    {
+                        rezult += Dict[item];
+                        ArabNumbers -= item;
+                        if (ArabNumbers == 1)
+                            break;
+                    }
+                }
+                return rezult;
+            }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int Translate(string RomaNumbers)
+            {
+            if (RomaNumbers == "")
+                return -1;
+            var Dicti = new Dictionary<char, int>
+                {
+                    { 'I', 1 },
+                    { 'V', 5 },
+                    { 'X', 10 },
+                    { 'L', 50 },
+                    { 'C', 100 },
+                    { 'D', 500 },
+                    { 'M', 1000 }
+                };
+                int summ, same = 0;
+                int sign = 1;
+                foreach (var item in RomaNumbers)
+                {
+                    if (!Dicti.ContainsKey(item))
+                    {
+                        Console.WriteLine("eror not roma in [1 - 1000]");
+                        return -1;
+                    }
+                }
+                summ = Dicti[RomaNumbers[^1]];
+                for (int k = RomaNumbers.Length - 1; k > 0; k--)
+                {
+                    if (Dicti[RomaNumbers[k - 1]] > Dicti[RomaNumbers[k]])
+                    {
+                        sign = 1;
+                        same = 0;
+                    }
+                    else if (Dicti[RomaNumbers[k - 1]] == Dicti[RomaNumbers[k]])
+                    {
+                        same++;
+                    }
+                    else
+                    {
+                        sign = -1;
+                        same = 0;
+                    }
+                    if (same != 0)
+                        same--;
+                    summ += Dicti[RomaNumbers[k - 1]] * sign + same * Dicti[RomaNumbers[k]] * sign;
+                }
+                return summ;
+            }
+    }
+}
